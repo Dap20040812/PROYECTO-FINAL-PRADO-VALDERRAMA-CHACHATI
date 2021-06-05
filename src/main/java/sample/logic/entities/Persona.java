5fd8acaf251f29e793acad7e28f90f4da3fb1bcf
Diego Prado <diegoprpr@unisabana.edu.co>
2021-06-04 23:19:07 -0500

    NO se cambio nada solo fue un error para hacer el branch

--- src/main/java/sample/logic/entities/Persona.java
+++ src/main/java/sample/logic/entities/Persona.java
@@ -18,8 +18,9 @@ public class Persona extends Exportable implements Serializable {
     private String status;
     private String description;
     private String photo;
+    private String gender;
 
-    public Persona(String name, String lastName, String age, String profession, String role, String status, String description, String photo) throws PersonaException {
+    public Persona(String name, String lastName, String age, String profession, String role, String status, String description, String photo, String gender) throws PersonaException {
         this.name = name;
         this.lastName = lastName;
         this.profession = profession;
@@ -27,6 +28,7 @@ public class Persona extends Exportable implements Serializable {
         this.status = status;
         this.description = description;
         this.photo = photo;
+        this.gender = gender;
         this.setAge(age);
     }
 
@@ -52,12 +54,19 @@ public class Persona extends Exportable implements Serializable {
         return status;
     }
 
+    public String getGender() {
+        return gender;
+    }
+
     public void setProfession(String profession) {
         this.profession = profession;
     }
 
     public void setRole(String role) {
         this.role = role;
+    }
+    public void setGender(String gender1) {
+        this.gender = gender1;
     }
 
     public String getPhoto() {
@@ -111,7 +120,7 @@ public class Persona extends Exportable implements Serializable {
     @Override
     public String toString() {
 
-        return String.format("Name = %s, LastName = %s, Age = %s, Profession = %s, Role = %s, Status = %s, Description = %s, Photo = %s", this.name, this.lastName, this.age, this.profession,this.role,this.status,this.description,this.photo);
+        return String.format("Name = %s, LastName = %s, Age = %s, gender = %s, Profession = %s, Role = %s, Status = %s, Description = %s, Photo = %s", this.name, this.lastName, this.age, this.gender, this.profession,this.role,this.status,this.description,this.photo);
     }
 
     @Override
@@ -120,11 +129,13 @@ public class Persona extends Exportable implements Serializable {
         result.add(this.name);
         result.add(this.lastName);
         result.add(this.getAge());
+        result.add(this.gender);
         result.add(this.profession);
         result.add(this.role);
         result.add(this.status);
         result.add(this.description);
         result.add(this.photo);
+
 
         return result;
     }
@@ -132,6 +143,6 @@ public class Persona extends Exportable implements Serializable {
     @Override
     public String getHeader() {
 
-        return "name,LastName,age,profession,role,status,description,photo";
+        return "name,LastName,age,gender,profession,role,status,description,photo";
     }
 }
